ee0f4a502962a6cd6d5c41c8b5ad58d49aec688a
Miguel Macedo Ferreira <mmmiguel_macedo@outlook.com>
2024-03-19 20:00:52 -0300

    Testes AVL, Tabela de Decisão e Partição por Equivalência.

--- src/test/java/GerenciadTarefasFunctionalTests/FunctionalTests.java
+++ src/test/java/GerenciadTarefasFunctionalTests/FunctionalTests.java
@@ -1,7 +1,5 @@
 package GerenciadTarefasFunctionalTests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import GerenciadorTarefas.TarefaController;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
